bb9112ba19acccd9213b32ddfc7b18bd5af94333
lokashakthivel-sp <lokashakthivelsp@gmail.com>
2025-10-04 23:10:40 +0530

    fix bug in server - fallback to index page

--- backend/server.js
+++ backend/server.js
@@ -41,10 +41,11 @@ const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("*any", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
+
 //
 /*global process */
 
